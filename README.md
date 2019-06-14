e14bcbbff8567dda693b2852461f6cdba10a76f6
hendisantika <hendisantika@yahoo.co.id>
2019-06-15 05:52:03 +0700

    #1 - Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-scheduler-example
---
+++ src/main/java/com/hendisantika/springbootschedulerexample/SpringbootSchedulerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootschedulerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSchedulerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSchedulerExampleApplication.class, args);
+    }
+
+}
